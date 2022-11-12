1a7d23f002f4d7ea4d4e8a6cea0409fb1f6d64aa
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2022-11-12 21:12:42 +0300

    pp: add hws that I'm not very proud of (code is not mine)

--- semester-1/procedural-programming/hw-4/random_number_gen.cpp
+++ semester-1/procedural-programming/hw-4/random_number_gen.cpp
@@ -1,42 +1,67 @@
-#include <iostream>
-#include <clocale>
-#include <random>
-
+﻿#include <iostream>
+#include <time.h>
 using namespace std;
 
-int main()
+void big()
 {
-	setlocale(LC_ALL, "Russian");
-	
-	int number_of_generations{};
-	cout << "Солько чисел мне сгенерировать? ";
-	cin >> number_of_generations;
-
-	int number_size{};
-	cout << "Какого размера сгенерировать числа? (1 - маленькие, 2 - большие) ";
-	cin >> number_size;
-
-	switch (number_size)
+	setlocale(0, "");
+	int m, i, c, S0, S1, b;
+	m = 25173;
+	c = 65537;
+	b = 13849;
+	cout << "i – количество чисел" << endl;
+	cin >> i;
+	S0 = time(0);
+	for (int j = 0; j < i; j++)
+	{
+		S1 = (m * S0 + b) % c;
+		cout << S1 << " ";
+		S0 = S1;
+	}
+}
+void small()
+{
+	setlocale(0, "");
+	int m, i, c, S0, S1, b;
+	m = 37;
+	c = 64;
+	b = 3;
+	cout << "i – количество чисел" << endl;
+	cin >> i;
+	S0 = time(0);
+	for (int j = 0; j < i; j++)
+	{
+		S1 = (m * S0 + b) % c;
+		cout << S1 << " ";
+		S0 = S1;
+	}
+}
+void main()
+{
+	setlocale(0, "");
+	cout << "Выберите какие числа вы хотите: " << endl;
+	cout << "1 - маленькие" << endl;
+	cout << "2 - большие" << endl;
+	double ch = 0;
+	int ch1;
+	do {
+		cin >> ch;
+		if (ch < 1 || ch > 2 || double(ch) - int(ch) != 0)
+		{
+			cout << "Неверный ввод. Попробуйте ещё раз: ";
+			cin.clear();
+			cin.ignore(1000000, '\n');
+		}
+		cout << endl;
+	} while (ch < 1 || ch > 2 || double(ch) - int(ch) != 0);
+	ch1 = int(ch);
+	switch (ch1)
 	{
 	case 1:
-		double m = 37;
-		double b = 3;
-		double c = 64;
-		
-		for (int i = 0; i < number_of_generations; i++)
-		{
-			double s = (m * )
-		}
-
+		small();
 		break;
 	case 2:
-		double m = 25173;
-		double b = 13849;
-		double c = 65537;
-
-		break;
-	default:
-		cout << "Я такое не умею" << endl;
+		big();
 		break;
 	}
 }
---
+++ semester-1/procedural-programming/hw-4/roman_dech.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Я не горд этим кодом. Он не мой
+
+int Decoder(string a) {
+	int j = a.length();
+	a += "000";
+	int NUMBER = 0;
+	int x = 0, y = 0, z = 0, t = 0,
+		kv = 0, kl = 0, kd = 0,
+		k2 = 0,
+		k3 = 1001;
+	for (int i = 0; i < j; i++)
+	{
+		switch (a[i])
+		{
+		case 48: y = 0; break;
+		case 73: x = 1; break;
+		case 86: x = 5; kv = kv + 1;  break;
+		case 88: x = 10; break;
+		case 76: x = 50; kl = kl + 1; break;
+		case 67: x = 100; break;
+		case 68: x = 500; kd = kd + 1; break;
+		case 77: x = 1000; break;
+		}
+		switch (a[i + 1])
+		{
+		case 48: y = 0; break;
+		case 73: y = 1; break;
+		case 86: y = 5; break;
+		case 88: y = 10; break;
+		case 76: y = 50; break;
+		case 67: y = 100; break;
+		case 68: y = 500; break;
+		case 77: y = 1000; break;
+		}
+		switch (a[i + 2])
+		{
+		case 48: z = 0; break;
+		case 73: z = 1; break;
+		case 86: z = 5; break;
+		case 88: z = 10; break;
+		case 76: z = 50; break;
+		case 67: z = 100; break;
+		case 68: z = 500; break;
+		case 77: z = 1000; break;
+		}
+		switch (a[i + 3])
+		{
+		case 48: t = 0; break;
+		case 73: t = 1; break;
+		case 86: t = 5; break;
+		case 88: t = 10; break;
+		case 76: t = 50; break;
+		case 67: t = 100; break;
+		case 68: t = 500; break;
+		case 77: t = 1000; break;
+		}
+		if (x < y)
+		{
+			NUMBER = NUMBER - x;
+			k3 = x;
+		}
+		else if (y < k3)
+		{
+			NUMBER = NUMBER + x;
+		}
+		else { k2 = k2 + 1; }
+		if (y - x == x) { k2 = k2 + 1; }
+		if ((x < z && y < z)
+			or (kl > 1) or (kd > 1) or (kv > 1)
+			or ((x == y) && (y == z) && (z == t) && (t == x))
+			or (k2 > 0))
+		{
+			cout << "Некорректная запись числа" << endl;
+			return (0);
+		}
+	}
+	return (NUMBER);
+}
+
+int main() {
+	setlocale(0, "");
+
+	string b;
+	cout << "Введите число:" << endl;
+	getline(cin, b);
+	cout << Decoder(b);
+	return 0;
+}
