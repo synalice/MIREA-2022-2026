9495be1a90d446049a846a7ae85c5b9ac9425f38
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2022-12-16 13:14:04 +0300

    sem1-pp-fix: (`hw-extra/111.cpp`) add totals number of permutations

--- Семестр 1/Процедурное программирование/hw-extra/111.cpp
+++ Семестр 1/Процедурное программирование/hw-extra/111.cpp
@@ -1,23 +1,28 @@
-#include <iostream>
 #include <clocale>
+#include <iostream>
 #include <vector>
 
-void Hanoi(int n, int from, int to) {
-	if (n > 0) {
-		int tmp = 6 - from - to;
-		Hanoi(n - 1, from, tmp);
-		printf("%d -> %d\n", from, to);
-		Hanoi(n - 1, tmp, to);
-	}
+void Hanoi(int n, int from, int to, int& total)
+{
+    if (n > 0) {
+        int tmp = 6 - from - to;
+        Hanoi(n - 1, from, tmp, total);
+        printf("%d -> %d\n", from, to);
+        total++;
+        Hanoi(n - 1, tmp, to, total);
+    }
 }
 
-int main() {
-	std::setlocale(LC_ALL, "ru_RU.UTF-8");
+int main()
+{
+    std::setlocale(LC_ALL, "ru_RU.UTF-8");
 
-	int n{};
-	std::cout << "Введите число колец: ";
-	std::cin >> n;
-	Hanoi(n, 1, 3);
+    int total {};
+    int n {};
+    std::cout << "Введите число колец: ";
+    std::cin >> n;
+    Hanoi(n, 1, 3, total);
+    std::cout << "Кол-во перестановок:\n" << total << std::endl;
 
-	return 0;
+    return 0;
 }
